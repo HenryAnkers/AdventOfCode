eac70115b0e0e95627c35dfefdab7d0a085b6635
HenryAnkers <henry.ankers@deliveroo.co.uk>
2022-12-01 10:58:11 +0000

    Template

---
+++ dayX.rb
@@ -0,0 +1,19 @@
+class DayX
+  def initialize
+    input = File.read('input/dayX.txt')
+
+    puts part1(input)
+    puts part2(input)
+  end
+
+
+  def part1(input)
+  end
+
+
+  def part2(input)
+  end
+end
+
+
+DayX.new
