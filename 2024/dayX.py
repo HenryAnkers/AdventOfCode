b90457e5e51948e5e8a8339b04c2c3c0524d2eb9
Henry Ankers <henry.ankers@samsara.com>
2024-12-10 10:12:34 +0000

    Template

--- 2024/dayX.py
+++ 2024/dayX.py
@@ -9,6 +9,16 @@ def parseIntInput(file_path):
     with open(file_path, 'r') as file:
         return [[int(item) for item in line.strip().split()] for line in file]
 
+def parseInputAsMap(file_path):
+    with open(file_path, 'r') as file:
+        data = [line.strip() for line in file]
+        dataMap = defaultdict(lambda: defaultdict(bool))
+        for y, line in enumerate(data):
+            for x, char in enumerate(line):
+                dataMap[x][y] = char
+        return dataMap
+
+
 def part1(data):
     return None
 
@@ -20,8 +30,9 @@ day = os.path.basename(__file__).split('.')[0].replace('day', '')
 input_path = f"./inputs/day{day}.txt"
 
 start_time = time.time()
-#input_data = parseInput(input_path)
-input_data = parseIntInput(input_path)
+input_data = parseInput(input_path)
+#input_data = parseInputAsMap(input_path)
+#input_data = parseIntInput(input_path)
 end_time = time.time()
 print(f"Day {day} - Parsing took {end_time - start_time:.6f} seconds")
 #print(input_data)
